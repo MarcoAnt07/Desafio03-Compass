126d25281906d78175df72c4c2fa394d24f83ec7
Marco Antonio <marcorodoi64@gmail.com>
2024-12-24 16:11:14 -0300

    /feat: Implementação operação GET /get-all-events

--- ms-event-manager/src/main/java/io/github/marcoant07/ms_event_manager/controller/EventController.java
+++ ms-event-manager/src/main/java/io/github/marcoant07/ms_event_manager/controller/EventController.java
@@ -2,6 +2,7 @@ package io.github.marcoant07.ms_event_manager.controller;
 
 import io.github.marcoant07.ms_event_manager.dto.Address;
 import io.github.marcoant07.ms_event_manager.dto.CreateEventDTO;
+import io.github.marcoant07.ms_event_manager.dto.GetEventDTO;
 import io.github.marcoant07.ms_event_manager.dto.mapper.Mapper;
 import io.github.marcoant07.ms_event_manager.entity.Event;
 import io.github.marcoant07.ms_event_manager.repository.EventRepository;
@@ -10,6 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("event")
@@ -34,5 +37,12 @@ public class EventController {
         Event savedEvent = eventRepository.save(event);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(savedEvent);
+    }
+
+    @GetMapping("/get-all-events")
+    public ResponseEntity<List<GetEventDTO>> getAllEvents(){
+        List<Event> events = eventRepository.findAll();
+
+        return ResponseEntity.status(HttpStatus.OK).body(Mapper.toListDTO(events));
     }
 }
--- ms-event-manager/src/main/java/io/github/marcoant07/ms_event_manager/dto/mapper/Mapper.java
+++ ms-event-manager/src/main/java/io/github/marcoant07/ms_event_manager/dto/mapper/Mapper.java
@@ -1,9 +1,15 @@
 package io.github.marcoant07.ms_event_manager.dto.mapper;
 
+import ch.qos.logback.core.model.Model;
 import io.github.marcoant07.ms_event_manager.dto.CreateEventDTO;
+import io.github.marcoant07.ms_event_manager.dto.GetEventDTO;
 import io.github.marcoant07.ms_event_manager.entity.Event;
 import io.github.marcoant07.ms_event_manager.repository.EventRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Mapper {
 
@@ -16,5 +22,13 @@ public class Mapper {
 
 
         return event;
+    }
+
+    public static GetEventDTO toEventDTO(Event event){
+        return new ModelMapper().map(event, GetEventDTO.class);
+    }
+
+    public static List<GetEventDTO> toListDTO(List<Event> events){
+        return events.stream().map(Mapper::toEventDTO).collect(Collectors.toList());
     }
 }

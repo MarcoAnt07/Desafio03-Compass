fb3216de1ae8e4769674275e1d7b03b6abc48e60
Marco Antonio <marcorodoi64@gmail.com>
2025-01-07 16:54:29 -0300

    /refactor: Melhoria no tratamento de erros

--- ms-event-manager/src/main/java/io/github/marcoant07/ms_event_manager/exception/ErrorHandler.java
+++ ms-event-manager/src/main/java/io/github/marcoant07/ms_event_manager/exception/ErrorHandler.java
@@ -6,9 +6,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class ErrorHandler {
@@ -25,5 +26,12 @@ public class ErrorHandler {
         ErrorMessage errorMessage = new ErrorMessage(request, HttpStatus.NOT_FOUND, e.getMessage());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).contentType(MediaType.APPLICATION_JSON).body(errorMessage);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> methodArgumenteNotValidException(MethodArgumentNotValidException e, HttpServletRequest request, BindingResult bindingResult){
+        ErrorMessage errorMessage = new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY, "Invalid Fields", bindingResult);
+
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).contentType(MediaType.APPLICATION_JSON).body(errorMessage);
     }
 }
--- ms-ticket-manager/src/main/java/io/github/marcoant07/ms_ticket_manager/exception/ErrorHandler.java
+++ ms-ticket-manager/src/main/java/io/github/marcoant07/ms_ticket_manager/exception/ErrorHandler.java
@@ -5,6 +5,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -16,5 +18,13 @@ public class ErrorHandler{
         ErrorMessage errorMessage = new ErrorMessage(request, HttpStatus.NOT_FOUND, e.getMessage());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).contentType(MediaType.APPLICATION_JSON).body(errorMessage);
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> methodArgumenteNotValidException(MethodArgumentNotValidException e, HttpServletRequest request, BindingResult bindingResult){
+        ErrorMessage errorMessage = new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY, "Invalid Fields", bindingResult);
+
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).contentType(MediaType.APPLICATION_JSON).body(errorMessage);
     }
 }
